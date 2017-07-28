f21eb0d0cd1b26a610b344b7f925d8addd7955ac
Jerry W <jerrywiltse@gmail.com>
2017-07-28 14:51:30 -0400

    updated recipe failing

--- conanfile.py
+++ conanfile.py
@@ -1,8 +1,7 @@
 """Conan.io recipe for Boost.Build
 """
 from conans import ConanFile, tools
-from os import path
-import subprocess
+
 
 class BoostBuildConan(ConanFile):
     """Checkout Boost.Build, build and create package
@@ -12,12 +11,12 @@ class BoostBuildConan(ConanFile):
     generators = "txt"
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/build"
+    src_folder = "git_src"
 
     def source(self):
-        self.run("git clone --depth=50 --branch=v%s %s.git %s" % (self.version, self.url, self.cpprestsdk_dir))
+        self.run("git clone --depth=50 --branch=v%s %s.git %s" % (self.version, self.url, self.src_folder))
 
     def build(self):
-      print(self.settings.os)
         command = "bootstrap" if self.settings.os == "Windows" else "./bootstrap.sh"
         flags = []
         if self.settings.os == "Windows" and self.settings.compiler == "gcc":
@@ -28,19 +27,15 @@ class BoostBuildConan(ConanFile):
         elif str(self.settings.compiler) in ["clang", "gcc"]:
             flags.append("toolset=%s"% self.settings.compiler)
 
-        flags.append("link=%s" % ("static" if not self.options.shared else "shared"))
-        if self.settings.compiler == "Visual Studio" and self.settings.compiler.runtime:
-            flags.append("runtime-link=%s" % ("static" if "MT" in str(self.settings.compiler.runtime) else "shared"))
-        flags.append("variant=%s" % str(self.settings.build_type).lower())
-        flags.append("address-model=%s" % ("32" if self.settings.arch == "x86" else "64"))
+        try:
+            self.run("cd %s && %s" % (self.src_folder, command))
+        except:
+            self.run("cd %s && type bootstrap.log" % self.src_folder
+                     if self.settings.os == "Windows"
+                     else "cd %s && cat bootstrap.log" % self.src_folder)
+            raise
 
-        
         cxx_flags = []
-        # fPIC DEFINITION
-        if self.settings.compiler != "Visual Studio":
-            if self.options.fPIC:
-                cxx_flags.append("-fPIC")
-
 
         # LIBCXX DEFINITION FOR BOOST B2
         try:
@@ -67,17 +62,12 @@ class BoostBuildConan(ConanFile):
 
         command = "b2" if self.settings.os == "Windows" else "./b2"
 
-        without_python = "--without-python" if not self.options.python else ""
-        full_command = "cd %s && %s %s -j%s --abbreviate-paths %s" % (
-            self.FOLDER_NAME,
+        full_command = "cd %s && %s %s --abbreviate-paths" % (
+            self.src_folder,
             command,
-            b2_flags,
-            tools.cpu_count(),
-            without_python)
+            b2_flags)
         self.output.warn(full_command)
-        
-        envs = self.prepare_deps_options_env()
-        with tools.environment_append(envs):
-            self.run(full_command)
+
+        self.run(full_command)
 
 
