9c44415b9bc062e5148aaa575d729e1020c37651
Jerry W <jerrywiltse@gmail.com>
2017-07-28 14:10:04 -0400

    starting on recipe

--- conanfile.py
+++ conanfile.py
@@ -11,16 +11,73 @@ class BoostBuildConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
     url = "https://github.com/boostorg/build"
 
     def source(self):
         self.run("git clone --depth=50 --branch=v%s %s.git %s" % (self.version, self.url, self.cpprestsdk_dir))
 
     def build(self):
+      print(self.settings.os)
+        command = "bootstrap" if self.settings.os == "Windows" else "./bootstrap.sh"
+        flags = []
+        if self.settings.os == "Windows" and self.settings.compiler == "gcc":
+            command += " mingw"
+            flags.append("--layout=system")
+        if self.settings.compiler == "Visual Studio":
+            flags.append("toolset=msvc-%s.0" % self.settings.compiler.version)
+        elif str(self.settings.compiler) in ["clang", "gcc"]:
+            flags.append("toolset=%s"% self.settings.compiler)
+
+        flags.append("link=%s" % ("static" if not self.options.shared else "shared"))
+        if self.settings.compiler == "Visual Studio" and self.settings.compiler.runtime:
+            flags.append("runtime-link=%s" % ("static" if "MT" in str(self.settings.compiler.runtime) else "shared"))
+        flags.append("variant=%s" % str(self.settings.build_type).lower())
+        flags.append("address-model=%s" % ("32" if self.settings.arch == "x86" else "64"))
+
+        
+        cxx_flags = []
+        # fPIC DEFINITION
+        if self.settings.compiler != "Visual Studio":
+            if self.options.fPIC:
+                cxx_flags.append("-fPIC")
 
 
-    def package(self):
+        # LIBCXX DEFINITION FOR BOOST B2
+        try:
+            if str(self.settings.compiler.libcxx) == "libstdc++":
+                flags.append("define=_GLIBCXX_USE_CXX11_ABI=0")
+            elif str(self.settings.compiler.libcxx) == "libstdc++11":
+                flags.append("define=_GLIBCXX_USE_CXX11_ABI=1")
+            if "clang" in str(self.settings.compiler):
+                if str(self.settings.compiler.libcxx) == "libc++":
+                    cxx_flags.append("-stdlib=libc++")
+                    cxx_flags.append("-std=c++11")
+                    flags.append('linkflags="-stdlib=libc++"')
+                else:
+                    cxx_flags.append("-stdlib=libstdc++")
+                    cxx_flags.append("-std=c++11")
+        except:
+            pass
+
+        cxx_flags = 'cxxflags="%s"' % " ".join(cxx_flags) if cxx_flags else ""
+        flags.append(cxx_flags)
+
+        # JOIN ALL FLAGS
+        b2_flags = " ".join(flags)
+
+        command = "b2" if self.settings.os == "Windows" else "./b2"
+
+        without_python = "--without-python" if not self.options.python else ""
+        full_command = "cd %s && %s %s -j%s --abbreviate-paths %s" % (
+            self.FOLDER_NAME,
+            command,
+            b2_flags,
+            tools.cpu_count(),
+            without_python)
+        self.output.warn(full_command)
+        
+        envs = self.prepare_deps_options_env()
+        with tools.environment_append(envs):
+            self.run(full_command)
 
 
-    def package_info(self):
