eced6813ab76d26509c3a28204d1ba7bb2f5091b
Jerry W <jerrywiltse@gmail.com>
2017-07-31 15:55:03 -0400

    path adjustments

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,26 @@
-"""Conan.io recipe for Boost.Build
-"""
 from conans import ConanFile, tools, os
 
 class BoostBuildConan(ConanFile):
-    """Checkout Boost.Build, build and create package
-    """
     name = "Boost.Build"
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/build"
     description = "Boost.Build makes it easy to build C++ projects, everywhere"
     license = "www.boost.org/users/license.html"
-    #build_folder_name = "boost_{0}".format(version.replace(".", "_"))
     settings = "os", "compiler", "build_type", "arch"
-
+    lib_short_name = "build"
+          
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def build(self):
         command = "bootstrap" if self.settings.os == "Windows" else "./bootstrap.sh"
-        build_path = os.path.join(os.getcwd(), "build")
-        vscmd_path = os.path.join(build_path, "src", "engine")
         
-        os.chdir(build_path)
+        build_dir = os.path.join(os.getcwd(), self.lib_short_name)
+        vscmd_path = os.path.join(build_dir, "src", "engine")
+        
+        os.chdir(build_dir)
         
         with tools.environment_append({"VSCMD_START_DIR": vscmd_path}):
             try:
