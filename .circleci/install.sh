c80c97938bfc068871f6a61c45fb50e6e8862a99
Jerry W <jerrywiltse@gmail.com>
2018-01-24 20:19:56 -0500

    try without pyenv on circleci

--- .circleci/install.sh
+++ .circleci/install.sh
@@ -6,14 +6,6 @@ set -x
 brew update || brew update
 brew install cmake || true
 
-if which pyenv > /dev/null; then
-    eval "$(pyenv init -)"
-fi
-
-pyenv install 2.7.10
-pyenv virtualenv 2.7.10 conan
-pyenv rehash
-pyenv activate conan
 
 pip install conan --upgrade
 pip install conan_package_tools bincrafters_package_tools
