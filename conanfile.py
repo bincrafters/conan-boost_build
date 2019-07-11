c08b227a92958221fc0196cbdab325d576517938
Rene Rivera <grafikrobot@gmail.com>
2019-07-10 19:25:09 -0500

    Update to latest b2 release, 4.0.0.

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import shutil
 
 class BoostBuildConan(ConanFile):
     name = "boost_build"
-    version = "1.70.0"
+    version = "4.0.0"
     url = "https://github.com/bincrafters/conan-boost_build"
     description = "boost_build makes it easy to build C++ projects, everywhere"
     license = "BSL-1.0"
@@ -19,35 +19,25 @@ class BoostBuildConan(ConanFile):
     exports_sources = "*.jam"
 
     def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version
-        # archive_name = "master"
-        for lib_short_name in self.lib_short_names:
-            tools.get(
-                "{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-        shutil.copyfile(
-            'os.jam',
-            os.path.join('build', 'src', 'util', 'os.jam'))
+        tools.get(
+            "https://github.com/boostorg/build/archive/{0}.tar.gz"
+            .format(self.version))
+        os.rename("build-" + self.version, 'b2')
 
     def build(self):
         use_windows_commands = os.name == 'nt'
-        command = "bootstrap" if use_windows_commands else "./bootstrap.sh"
-        build_dir = os.path.join(self.source_folder, "build")
-        vscmd_path = os.path.join(build_dir, "src", "engine")
+        command = "build" if use_windows_commands else "./build.sh"
+        build_dir = os.path.join(self.source_folder, "b2")
+        engine_dir = os.path.join(build_dir, "src", "engine")
+        os.chdir(engine_dir)
+        with tools.environment_append({"VSCMD_START_DIR": os.curdir}):
+            self.run(command)
         os.chdir(build_dir)
-
-        with tools.environment_append({"VSCMD_START_DIR": vscmd_path}):
-            try:
-                self.run(command)
-            except:
-                read_cmd = "type" if use_windows_commands else "cat"
-                self.run("{0} bootstrap.log".format(read_cmd))
-                raise
-
-        command = "b2" if use_windows_commands else "./b2"
-        full_command = "{0} --prefix=../output --abbreviate-paths".format(command)
+        command = os.path.join(
+            engine_dir, "b2.exe" if use_windows_commands else "b2")
+        full_command = \
+            "{0} --prefix=../output --abbreviate-paths install".format(
+                command)
         self.run(full_command)
 
     def package(self):
@@ -57,5 +47,7 @@ class BoostBuildConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.bindirs = ["bin"]
-        self.env_info.path = [os.path.join(self.package_folder, "bin")] + self.env_info.path
-        self.env_info.BOOST_BUILD_PATH = os.path.join(self.package_folder, "share", "boost-build", "src", "kernel")
+        self.env_info.path = [os.path.join(
+            self.package_folder, "bin")] + self.env_info.path
+        self.env_info.BOOST_BUILD_PATH = os.path.join(
+            self.package_folder, "share", "boost-build", "src", "kernel")
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from conans import ConanFile, tools
+import os
 
 
 class TestPackgeConan(ConanFile):
     settings = "os", "arch"
-    
+
     def test(self):
-        tools.save("jamroot.jam", "ECHO info: Success loading project jamroot.jam file. ;")
+        tools.save(
+            "jamroot.jam",
+            'ECHO "info:" Success loading project jamroot.jam file. ;')
+        tools.save(
+            "boost-build.jam",
+            "boost-build \"" +
+            os.environ['BOOST_BUILD_PATH'].replace("\\", "/")+"\" ;"
+        )
         self.run("b2 --debug-configuration")
